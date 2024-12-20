03e6c8a1aa0e20245578af9543b15d18dbc75374
Benjamin <benjamin@BenjamindeMac-mini.local>
2024-12-20 11:15:58 +0400

    修改dashboard页面

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,31 +1,42 @@
 <template>
   <div class="dashboard-container">
-    <component :is="currentRole" />
+    <div class="welcome-message">
+      <h1>欢迎登陆</h1>
+      <p>请在左侧菜单栏中，进行操作。</p>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import adminDashboard from './admin'
-import editorDashboard from './editor'
-
 export default {
-  name: 'Dashboard',
-  components: { adminDashboard, editorDashboard },
-  data() {
-    return {
-      currentRole: 'adminDashboard'
-    }
-  },
-  computed: {
-    ...mapGetters([
-      'roles'
-    ])
-  },
-  created() {
-    if (!this.roles.includes('admin')) {
-      this.currentRole = 'editorDashboard'
-    }
-  }
+  name: 'Dashboard'
 }
 </script>
+
+<style scoped>
+.dashboard-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f0f2f5;
+}
+
+.welcome-message {
+  text-align: center;
+  background: #fff;
+  padding: 50px;
+  border-radius: 10px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+}
+
+.welcome-message h1 {
+  font-size: 36px;
+  margin-bottom: 20px;
+}
+
+.welcome-message p {
+  font-size: 18px;
+  color: #666;
+}
+</style>
